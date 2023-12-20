41f264cdd8ef7c868ff8a6c74efd55cc60010084
rumanstheddy <sumanthcob@gmail.com>
2023-12-19 21:00:09 -0500

    Updates

---
+++ precache-manifest.56434dcd6d2dda75a9f22020b385dee2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a217255d568a73f22e66118121af1740",
+    "url": "/my-reactfolio/index.html"
+  },
+  {
+    "revision": "1e86ebc19f84d79ff0af",
+    "url": "/my-reactfolio/static/css/main.2ce285ef.chunk.css"
+  },
+  {
+    "revision": "997188af9cd75a15a0c4",
+    "url": "/my-reactfolio/static/js/2.0a3d6390.chunk.js"
+  },
+  {
+    "revision": "2801257500996d2774f29ef0e1157fe6",
+    "url": "/my-reactfolio/static/js/2.0a3d6390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e0bf0cb8668e677bd6b",
+    "url": "/my-reactfolio/static/js/3.205a51e6.chunk.js"
+  },
+  {
+    "revision": "1e86ebc19f84d79ff0af",
+    "url": "/my-reactfolio/static/js/main.f22617c1.chunk.js"
+  },
+  {
+    "revision": "f305c0b64238473dc9b4",
+    "url": "/my-reactfolio/static/js/runtime-main.8655075f.js"
+  }
+]);
