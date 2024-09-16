5d9a3f7b930663fc48ca88ee81ae8f71e96ef163
rumanstheddy <sumanthcob@gmail.com>
2024-09-16 12:10:16 -0400

    Updated resume

--- src/data/user.js
+++ src/data/user.js
@@ -15,7 +15,7 @@ const defaultbgColor = "141414";
 const INFO = {
 	main: {
 		title: "Sumanth Chinnaobireddy",
-		name: "Tharindu N.",
+		name: "Sumanth Reddy",
 		email: "sumanthcob@gmail.com",
 		logo: "../logo.png",
 	},
@@ -39,7 +39,7 @@ const INFO = {
 		description:
 			"Dive into my journey – from academic milestones to professional endeavours. Discover my story in a resumé format.",
 		download: {
-			link: "https://dl.dropboxusercontent.com/scl/fi/piecj287psmll1hq3z77c/Resume2024-8.pdf?rlkey=l6tb15nseflk0ms9kh2dmuhts&st=io6fpnqy&dl=0",
+			link: "https://dl.dropboxusercontent.com/scl/fi/zldmqdikakd9nnqqtsznm/Resume2024-10.pdf?rlkey=zl3daybuuw6vsrchw9i7n14hq&st=91642o4t&dl=0",
 			title: "My Resumé",
 		},
 	},
