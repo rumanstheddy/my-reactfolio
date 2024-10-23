d18ee5916578b44657d30835f503b041252fea3a
Sumanth Reddy <sumanthcob@gmail.com>
2024-10-23 10:31:40 -0400

    updated resume link

--- src/data/user.js
+++ src/data/user.js
@@ -39,7 +39,7 @@ const INFO = {
 		description:
 			"Dive into my journey – from academic milestones to professional endeavours. Discover my story in a resumé format.",
 		download: {
-			link: "https://dl.dropboxusercontent.com/scl/fi/5ev0buwzbbrdfe2zivzq5/Resume2024-12.pdf?rlkey=j5llegwxu6m6sdu9iez81jnjd&st=9rqfv9c9&dl=0",
+			link: "https://dl.dropboxusercontent.com/scl/fi/3i4fn68behopyxqon8av4/Resume2024-13.pdf?rlkey=nra76zki02rqxorxx9ngwcp4f&st=gmirrp98&dl=0",
 			title: "My Resumé",
 		},
 	},
